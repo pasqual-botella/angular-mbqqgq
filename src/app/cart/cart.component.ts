a9fb7894fa75d11ac944c654c53c4e3fb0f08a25
pasqual-botella <pasqual.martinez@goumh.umh.es>
2022-06-06 13:40:51 +0200

    Using Forms for User Input

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -7,9 +9,25 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void{
+    //Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submited', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
